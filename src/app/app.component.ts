08de510013cbc8f67aa8cfbeec40a9abb3917b4c
unknown <med.benali.pro@gmail.com>
2020-03-03 09:00:32 +0100

    ajouter lazy loading et internationalisation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { TranslateService } from '@ngx-translate/core';
+import { I18nComponent } from './i18n/container/i18n.component';
+import * as fromI18n from './i18n/reducers';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
-export class AppComponent {
-  title = 'frontSide';
+export class AppComponent extends I18nComponent {
+  constructor(
+    readonly store: Store<fromI18n.State>,
+    readonly translate: TranslateService
+  ) {
+    super(store, translate);
+  }
 }
